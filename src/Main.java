9e1a5263e86b2db7379c92fdd392b2048e1a4e31
Anna Molodtsova <renasafetysea@mail.ru>
2022-08-18 22:44:50 +0300

    попытка номер 4 )

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,45 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {generateRandomArrow();}
+    public static void main(String[] args) {
+        java.util.Random random = new java.util.Random();
 
-        public static int[] generateRandomArrow() {
+        int arr[] = new int[30];
+        int sum = 0;
 
-            java.util.Random random = new java.util.Random();
-            int arr[] = new int[30];
-            int sum = 0;
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = random.nextInt(100_000) + 100_000;
-                sum += arr[i];
-                System.out.println("Сумма трат за месяц составила " + arr[i] + "рублей");
-                System.out.println(sum);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            sum += arr[i];
 
-                int max = arr[0];
-                int min = arr[0];
-                max = Math.max(max, arr[i]);
-                min = Math.min(min, arr[i]);
-                System.out.println("Max: " + max);
-                System.out.println("Min: " + min);
+            System.out.println("Сумма трат за месяц составила " + arr[i] + " рублей");
+            System.out.println(sum);
 
-            }
-            System.out.println("Среднее арифметическое всех элементов: " + sum / arr.length);
+            int max = arr[0];
+            int min = arr[0];
 
-                char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
 
-                for (int i = reverseFullName.length - 1; i >= 0; i--)
-                    System.out.print(reverseFullName[i]);
+            System.out.println("Max: " + max);
+            System.out.println("Min: " + min);
 
-            return arr;
         }
+        System.out.println("Среднее арифметическое всех элементов: " + sum / arr.length);
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = reverseFullName.length - 1; i >= 0; i--)
+            System.out.print(reverseFullName[i]);
     }
+
+    public static int[] generateRandomArrow() {
+
+        java.util.Random random = new java.util.Random();
+        int arr[] = new int[30];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = random.nextInt(100_000) + 100_000;
+
+        return arr;
+    }
+}
